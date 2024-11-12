05b537b6eff5abd4be9db469bdc78ba55b6b0822
Joanna Sela Rasmussen <joannarasmussen@mbptilhdejoanna.students.clients.local>
2024-11-12 11:58:10 +0100

    Small changes to the different summaries

--- scripts/clustering_test1.py
+++ scripts/clustering_test1.py
@@ -1,10 +1,10 @@
-
+import os
 import pandas as pd
 
-checkin_checkout_df = pd.read_csv('/Users/joannarasmussen/Documents/DTU/02807/Project/final_project_data_science/source/checkin_checkout_history_updated.csv')
-gym_location_df = pd.read_csv('/Users/joannarasmussen/Documents/DTU/02807/Project/final_project_data_science/source/gym_locations_data.csv')
-subscription_df = pd.read_csv('/Users/joannarasmussen/Documents/DTU/02807/Project/final_project_data_science/source/subscription_plans.csv')
-user_df = pd.read_csv('/Users/joannarasmussen/Documents/DTU/02807/Project/final_project_data_science/source/users_data.csv')
+checkin_checkout_df = pd.read_csv(os.path.join('..', 'source', 'checkin_checkout_history_updated.csv'))
+gym_location_df = pd.read_csv(os.path.join('..', 'source', 'gym_locations_data.csv'))
+subscription_df = pd.read_csv(os.path.join('..', 'source', 'subscription_plans.csv'))
+user_df = pd.read_csv(os.path.join('..', 'source', 'users_data.csv'))
 
 checkin_checkout_df.drop(['calories_burned'], axis = 1, inplace = True) # Remove 'calories_burned' column - not interested to look at
 
@@ -24,21 +24,19 @@ checkin_checkout_df['time_of_day'] = pd.cut(
 )
 checkin_checkout_df.drop(['time_window'], axis = 1, inplace = True) # Remove 'time_window' column - not interested to look at
 checkin_checkout_df = checkin_checkout_df.merge(user_df[['user_id', 'age', 'gender']], on = 'user_id', how = 'left')
-#print(checkin_checkout_df.head(25))
+
 
 # Groupby gym and extract most frequent item 
 gym_summary = checkin_checkout_df.groupby('gym_id').agg({
     'time_of_day': lambda x: x.mode()[0],  # Most common time window
     'workout_type': lambda x: x.mode()[0],  # Most common workout type
     'workout_duration (min)': 'mean',  # Average workout duration
-    'gender': lambda x: x.mode()[0]
+    'gender': lambda x: x.mode()[0] # Most common gender
 }).reset_index()
-
 gym_summary.columns = ['gym_id', 'peak_time_of_day', 'popular_workout_type', 'avg_workout_duration', 'most_common_gender']
-print(gym_summary.head(25))
 
+# Groupby gender and extract most frequent item
 gender_summary = checkin_checkout_df.groupby('gender').agg({
-    'age': 'mean', 
-    'workout_type': lambda x: x.mode()[0] 
+    'age': 'mean', # Average age
+    'workout_type': lambda x: x.mode()[0] # Most common workout type
 }).reset_index()
-print(gender_summary)
