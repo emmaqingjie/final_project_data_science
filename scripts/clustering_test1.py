72d8a12dd6a94afb81c420fa48e52066e367db2f
Joanna Sela Rasmussen <joannarasmussen@mbptilhdejoanna.students.clients.local>
2024-11-12 10:35:54 +0100

    Small changes

--- scripts/clustering_test1.py
+++ scripts/clustering_test1.py
@@ -8,9 +8,7 @@ gym_location_df = pd.read_csv('gym_locations_data.csv')
 subscription_df = pd.read_csv('subscription_plans.csv')
 user_df = pd.read_csv('users_data.csv')
 
-# JOIN dataframes
-
-# Calculate workout duration
+# Calculate workout length for each session
 checkin_checkout_df['workout_duration (min)'] = (pd.to_datetime(checkin_checkout_df['checkout_time']) - 
                                         pd.to_datetime(checkin_checkout_df['checkin_time'])).dt.total_seconds() / 60  # in minutes
 
@@ -22,13 +20,12 @@ checkin_checkout_df['time_of_day'] = pd.cut(
     include_lowest = True
 )
 
-# Data by gym
+# Groupby gym and extract most frequenct item 
 gym_summary = checkin_checkout_df.groupby('gym_id').agg({
     'time_of_day': lambda x: x.mode()[0],  # Most common time window
     'workout_type': lambda x: x.mode()[0],  # Most common workout type
     'workout_duration (min)': 'mean',  # Average workout duration
-    'user_id': 'count',  # Total check-ins (proxy for activity level)
+    'user_id': 'count',  # Total check-ins 
 }).reset_index()
 
 gym_summary.columns = ['gym_id', 'peak_time_of_day', 'popular_workout_type', 'avg_workout_duration', 'total_checkins']
-print(gym_summary)
